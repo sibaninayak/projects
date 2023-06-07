56904b128437f9471444e44d45acf6064856df8d
sibaninayak <sibaninayak1808@gmail.com>
2023-06-07 14:42:37 +0530

    first commit

---
+++ clock/script.js
@@ -0,0 +1,20 @@
+// let deg = 6;
+
+const hour = document.querySelector('.hour'),
+      minutes = document.querySelector('.minutes')
+      seconds = document.querySelector('.seconds')
+      
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
